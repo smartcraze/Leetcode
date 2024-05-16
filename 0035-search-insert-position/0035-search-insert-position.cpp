78aea8775251080f8cca8d3d4ad3e15ced310b78
Suraj Vishwakarma <smartcraze354@gmail.com>
2024-05-16 21:14:15 +0530

    Time: 3 ms (70.85%), Space: 12.1 MB (45.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,15 +7,14 @@ public:
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                return mid; 
+                return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1; 
+                left = mid + 1;
             } else {
-                right = mid - 1; 
+                right = mid - 1;
             }
         }
 
-        
         return left;
     }
 };
