d98905d58ed2591b1eb290dc58ea8f4ebe71b285
Suraj Vishwakarma <smartcraze354@gmail.com>
2025-02-18 12:26:12 +0530

    Time: 1 ms (72.81%), Space: 44.7 MB (58.9%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i=0;i<s.length();i++){
+            sb.setCharAt(indices[i], s.charAt(i));
+        }
+        return sb.toString();
+        
+    }
+        
+    
+}
